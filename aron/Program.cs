95a60b4706cfb5470d5ce6cc487d4e5b9d60e2ea
Mayeli <19895136@clases.edu.sv>
2026-04-15 08:00:05 -0600

    mayeli

--- aron/Program.cs
+++ aron/Program.cs
@@ -11,6 +11,7 @@ namespace aron
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hola indel");
         }
     }
 }
